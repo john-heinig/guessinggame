9bc5fd835be472cb734e7879766b1a08b27ae867
Heinig <413325@mcpsmd.org>
2019-11-22 08:47:32 -0500

    finished

--- randomGuess.py
+++ randomGuess.py
@@ -1,14 +1,14 @@
 import random
 rand = random.randrange(0,10)
-guess = int(input("what is your guess"))
+guess = int(input("what is your guess "))
 numguesses = 0
 while (rand!= guess):
     if(rand > guess):
-        print ("guess a higher number")
+        print ("guess a higher number ")
     else:
-        print("guess a lower number")
-    guess = int(input("guess again"))
+        print("guess a lower number ")
+    guess = int(input("guess again "))
     numguesses = numguesses + 1
 if (rand == guess):
     print ("you got the number, Nice job!")
-    print ("you did it in" + numguesses "turns!")
+    
